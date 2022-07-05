bfb6e2f0417f33ff1fb2efab0d3e980d48ae00c2
shane <shanestevensxk9@gmail.com>
2022-07-05 08:49:58 +0200

    update

--- java/code.js
+++ java/code.js
@@ -1,4 +1,22 @@
 const app = Vue.createApp({
+
+template: `
+<form class="text-center">
+  
+<label for="name">Name:</label><br>
+<input class="text-center" type="text" id="name" placeholder="Enter your Name"><br>
+<hr>
+<label for="age">Age:</label><br>
+<input class="text-center" type="text" id="age" placeholder="Enter your age"><br>
+<hr>
+<button v-on:click="getUser()">Display</button>
+
+<br><br>
+<p id="output" v-show="isVisible">My Name is {{firstName}} and I am {{age}} years old.</p>
+
+</form>
+`,
+
     data(){
         return{
 firstName: "...",
